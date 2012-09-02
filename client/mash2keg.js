793bcea2a74b55f683848928cc410e2d5d859e95
Den Lipovsky <den@denlipovsky.com>
2012-09-01 21:37:22 -0400

    better formatting of dates edit and delete on the list page

--- client/mash2keg.js
+++ client/mash2keg.js
@@ -3,11 +3,32 @@ var formula = function() {
     return Formulas.findOne(Session.get('formula_id'));
 };
 
+var formatDatePart = function(part) {
+    if(part < 10) { return "0" + part; }
+    else { return "" + part; }
+};
+Handlebars.registerHelper("formatDate", function(date) {
+    var date = new Date(date);
+    return date.getFullYear() + "-" + formatDatePart(date.getMonth() + 1) + "-" + formatDatePart(date.getDay()) + " " + date.getHours() + ":" + date.getMinutes()
+});
+Handlebars.registerHelper("formatDateWithSeconds", function(date) {
+    var date = new Date(date);
+    return date.getFullYear() + "-" + formatDatePart(date.getMonth() + 1) + "-" + formatDatePart(date.getDay()) + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds()
+});
 Template.page.preserve({
     'input[id]': function (n) { 
       return n.id; 
     },
     'textarea#mash-notes': true
+});
+
+Template.page.events({
+    'click a#list': function() {
+        Session.set('formula_id', undefined);
+    },
+    'click a#delete': function() {
+        Formulas.remove(this._id);
+    }
 });
 
 Template.formulaList.show = function() {
@@ -22,6 +43,9 @@ Template.formulaList.events({
     'click button#addFormula': function() {
         var formula_id = Formulas.insert({'created': new Date()});
         Session.set('formula_id', formula_id);
+    },
+    'click a#edit': function() {
+        Session.set('formula_id', this._id);
     }
 });
 
