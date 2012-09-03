b843fea0df3acbefa118c2d41ad2cfc2a502ea96
Den Lipovsky <den@denlipovsky.com>
2012-09-02 20:02:52 -0400

    gravity reading section

--- client/mash2keg.js
+++ client/mash2keg.js
@@ -290,5 +290,31 @@ Template.boil.events({
     }
 });
 
+Template.gravity.fermentationData = function() {
+    var fermentationData = formula().fermentationData || {};
+    if(fermentationData) {
+    }
+
+    return fermentationData;
+};
+
+Template.gravity.events({
+    'keyup input#fermentation-og': function(evt) {
+        var gravity = $('#gravity input#fermentation-og').val().trim();
+        var temp = $('#gravity input#fermentation-og-temp').val().trim();
+        var fermentationData = formula().fermentationData || {};
+        fermentationData.original = fermentationData.original || {};
+        fermentationData.og.reading = gravity;
+        fermentationData.og.temp = temp;
+        Formulas.update(formula()._id, {$set: {fermentationData: fermentationData}});
+    },
+    'keyup input#fermentation-fg': function(evt) {
+        var gravity = $('#gravity input#fermentation-fg').val().trim();
+        var fermentationData = formula().fermentationData || {};
+        fermentationData.finalGravity = gravity;
+        Formulas.update(formula()._id, {$set: {fermentationData: fermentationData}});
+    }
+});
+
 Meteor.startup(function() {
 });
