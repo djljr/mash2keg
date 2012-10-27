1aac7fc5d7344c9627b5ed74a9d1735eb5e14d68
Den Lipovsky <den@denlipovsky.com>
2012-10-27 18:44:40 -0400

    removing the link to delete recipes, do this through the console for now fixed allowAdmin to work on the deployed app

--- model.js
+++ model.js
@@ -1,6 +1,6 @@
 Formulas = new Meteor.Collection("formulas");
 var allowAdmin = function(userId, doc) {
-    var user = Meteor.users().findOne(userId);
+    var user = Meteor.users.findOne(userId);
     var isAdmin = user.admin;
     if(typeof(isAdmin) !== 'undefined') {
         return isAdmin;
